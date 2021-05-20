e86a01cd0c9863636cce90e12e0ea7bbd13d6b10
Unnikrishnan N P <uknp0371@gmail.com>
2021-05-20 17:48:32 +0530

    Updated home-page

--- event_portal/events/models.py
+++ event_portal/events/models.py
@@ -11,3 +11,20 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class EventStatus(models.Model):
+    STATUS = (
+        ('Ongoing Events', 'Ongoing Events'),
+        ('Events Conducted', 'Events Conducted'),
+        ('Upcoming Events', 'Upcoming Events')
+    )
+    # ongoing_events = models.CharField(max_length=200)
+    # events_conducted = models.CharField(max_length=200, null=True)
+    # upcoming_events = models.CharField(max_length=200)
+    events_created = models.ForeignKey(
+        Event, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    def __str__(self):
+        return self.status
--- event_portal/events/views.py
+++ event_portal/events/views.py
@@ -5,12 +5,21 @@ from .models import *
 
 
 def home(request):
-    return render(request, 'events/dashboard.html')
+    event = Event.objects.all()
+    event_status = EventStatus.objects.all()
+    ongoing_events = event_status.filter(status='Ongoing Events').count()
+    events_conducted = event_status.count()
+    upcoming_events = event_status.filter(status='Upcoming Events').count()
+
+    context = {'events': event, 'ongoing_events': ongoing_events,
+               'events_conducted': events_conducted, 'upcoming_events': upcoming_events}
+
+    return render(request, 'events/dashboard.html', context)
 
 
 def events(request):
     event = Event.objects.all()
-    return render(request, 'events/events.html', {'events': events})
+    return render(request, 'events/events.html', {'events': event})
 
 
 def tba(request):
